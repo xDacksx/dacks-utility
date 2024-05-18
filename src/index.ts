ab858962c4a269bec1eeda4655e230051170af64
Diego Zamora <diegozar02@gmail.com>
2024-05-17 21:33:43 -0600

    add isEmail function to global exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { capitalizeFirstChar, formatArrayAsSentence } from "./strings";
+import { capitalizeFirstChar, formatArrayAsSentence, isEmail } from "./strings";
 import { addDays, isPast, parseDateString } from "./dates";
 import { SuccessMessage, WarnMessage } from "./message";
 import { ErrorMessage, InfoMessage } from "./message";
 import { Generate, Hash } from "./password";
 
-export { capitalizeFirstChar, formatArrayAsSentence };
+export { capitalizeFirstChar, formatArrayAsSentence, isEmail };
 export { addDays, isPast, parseDateString };
 export { SuccessMessage, WarnMessage };
 export { ErrorMessage, InfoMessage };
