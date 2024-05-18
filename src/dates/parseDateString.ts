a6314272f5aad83d6c0cd44ba69a5565a08fc67b
Diego Zamora <diegozar02@gmail.com>
2024-05-17 21:20:13 -0600

    create parseDateString function

---
+++ src/dates/parseDateString.ts
@@ -0,0 +1,16 @@
+export function parseDateString(dateString: string): Date {
+    const regex =
+        /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+
+    if (!regex.test(dateString)) throw new Error("Format must be (dd-mm-yyyy)");
+
+    const parts = dateString.split("-");
+
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+
+    const date = new Date(year, month, day);
+
+    return date;
+}
